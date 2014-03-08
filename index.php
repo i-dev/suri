09459a241b51f1a626964948505f2fb04f9f27e0
Thomas <thomas.delemis@i-dev.fr>
2014-03-08 18:40:27 +0100

    cleaning

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 $loader = require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
@@ -10,17 +9,7 @@ require_once __DIR__.'/app/bootstrap.php';
 $app['debug'] = false;
 
 
-
-
-/*************** Fin de configuratiuon de l'application ***************/
-/*********************************************************************/
-/*************** Déclaration des controlleurs ***************/
-
 include __DIR__.'/src/Idev/Controller/controllers_backend.php';
 include __DIR__.'/src/Idev/Controller/controllers_frontend.php';
-
-/*************** Fin de déclaration des controlleurs ***************/
-/*******************************************************************/
-/*************** Lancement de l'application ***************/
 
 $app->run();
